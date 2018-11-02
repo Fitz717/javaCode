dead30a454570d0d53e676ca873e6a62221e25a5
Your Name <you@example.com>
2018-11-02 18:08:28 +0800

    可扩展数组

---
+++ ExplodeArray.java
@@ -0,0 +1,52 @@
+
+public class ExplodeArray {
+
+	public static void main(String[] args) {
+		
+		AddArray<String> s1=new AddArray<String>(20);
+		s1.add("1");
+		s1.add("2");
+		s1.add("3");
+		System.out.println(s1);
+	}
+
+}
+
+class AddArray<E>{
+	
+	private Object[] elementData;
+	private int size;
+	
+	private static final int DEFAULT_CAPACITY=10;
+	
+	public AddArray() {
+		elementData=new Object[DEFAULT_CAPACITY];
+	}
+	public AddArray(int capatity) {
+		elementData =new Object[capatity];	
+	}
+	public void add(E element) {
+		elementData[size++]=element;
+		if(size==elementData.length) {
+			Object[] newArray=new Object[elementData.length+elementData.length/2];
+			System.arraycopy( elementData, 0,newArray, 0 ,elementData.length);
+			elementData=newArray;
+		}
+	
+	}
+		public String toString(){
+		
+		StringBuilder sb =new StringBuilder();
+		sb.append("[");
+		
+		for(int i=0;i<size;i++) 
+			sb.append(elementData[i]+",");
+		
+		sb.setCharAt(sb.length()-1, ']');
+		return sb.toString();
+		
+	}
+
+	}
+	
+	
